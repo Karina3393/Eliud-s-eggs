70ba8a2774cec09e8518f5a613e9007c6a15a8d7
карина <@DESKTOP-FP9I0KA>
2024-09-26 14:51:10 +0300

    Add project files.

---
+++ Eliud's eggs/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Eliud_s_eggs
+{
+    internal class Program;
+    class ChickenCoop
+    {
+
+        public static int CountEggs(int number)
+        {
+            int count = 0;
+
+
+            while (number > 0)
+            {
+
+                if ((number & 1) == 1)
+                {
+                    count++;
+                }
+
+                number >>= 1;
+            }
+
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int number1 = 89;
+            int number2 = 16;
+
+
+            Console.WriteLine("Number of eggs in the coop (example 1): " + CountEggs(number1));
+            Console.WriteLine("Number of eggs in the coop (example 2): " + CountEggs(number2));
+        }
+    }
+}
